3a39f4305bd4dab06fef6e98b143154cffb73502
DanArcherOnline <danarcheronline@gmail.com>
2021-02-20 19:24:40 +0900

    🐛 revert App component back to normal state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import './components/counter-display/counter-display.component';
-import CounterDisplay from './components/counter-display/counter-display.component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{ width: 700, height: 200 }}>
-          <CounterDisplay count="2" />
-        </div>
+        Counter
       </header>
     </div>
   );
