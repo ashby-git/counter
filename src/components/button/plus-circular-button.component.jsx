13e650fecea17191dba14ff4c3eeac82ae521f02
Ashby McFall <ashbymcfall@Ashbys-MacBook-Pro.local>
2021-03-27 16:08:05 +0900

    💄 refactor button style & add button components

---
+++ src/components/button/plus-circular-button.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CircularButton from './circular-button.component'
+
+import './circular-button.styles.scss';
+
+const PlusCircularButton = (props) => (
+
+    <CircularButton>
+        <i class="fas fa-plus"></i>
+    </CircularButton>
+
+)
+
+export default PlusCircularButton;
---
+++ src/components/button/refresh-circular-button.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CircularButton from './circular-button.component'
+
+import './circular-button.styles.scss';
+
+const RefreshCircularButton = (props) => (
+
+    <CircularButton className='button'>
+        <i class="fas fa-sync-alt"></i>
+    </CircularButton>
+
+)
+
+export default RefreshCircularButton;
